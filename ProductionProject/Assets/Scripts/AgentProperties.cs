f427be44fcb15703255bb7310638eb796e420972
LamIrvin <irvin.chris.lam@gmail.com>
2020-01-10 13:09:46 -0500

    added Probuilder

---
+++ ProductionProject/Assets/Scripts/AgentProperties.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/AgentProperties", order = 1)]
+public class AgentProperties : ScriptableObject
+{
+    public float MovementSpeed;
+    public float AttackSpeed;
+    public float Health;
+    public float Stamina;
+
+}
